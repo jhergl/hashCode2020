03bdd518e3f3379b0b5b689160ec1f0123fbbf7b
olmatuni <olmatuni@LES004886.corp.capgemini.com>
2020-02-20 20:20:48 +0100

    g0

--- hashCode2020/src/hashCode2020/Utils.java
+++ hashCode2020/src/hashCode2020/Utils.java
@@ -1,5 +1,7 @@
 package hashCode2020;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,13 +10,23 @@ import java.util.List;
 
 public class Utils {
 	
-	public static void writeFile(String path, List<String> lines) {
-		Path file = Paths.get(path);
-		try {
-			Files.write(file, lines, Charset.forName("UTF-8"));
-		} catch (IOException e) {
-			e.printStackTrace();
+	public static void writeFile(List<LibreriaSolucion> lines,String tipo) throws FileNotFoundException {
+	
+		PrintWriter escribir= new PrintWriter(tipo);
+		escribir.println(lines.size());
+		
+		for (int i = 0; i < lines.size(); i++) {
+			
+			escribir.println(lines.get(i).getIndiceLibreria()+" "+lines.get(i).getLibros().length);
+			
+			for (int j = 0; j < lines.get(i).getLibros().length; j++) {
+				escribir.print(lines.get(i).getLibros()[j]);
+			}
+			escribir.println();
+			
 		}
+		
+		
 	}
 
 
