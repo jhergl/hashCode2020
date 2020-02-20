bbd98f40941e9b7225301d0d96b3d69e321e174f
jesherna <jesherna@LES001671.corp.capgemini.com>
2020-02-20 20:21:33 +0100

    solucionLineal

--- hashCode2020/src/hashCode2020/Utils.java
+++ hashCode2020/src/hashCode2020/Utils.java
@@ -4,7 +4,12 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Utils {
 	
@@ -16,6 +21,33 @@ public class Utils {
 			e.printStackTrace();
 		}
 	}
-
-
+	
+	
+	
+	public List<LibreriaSolucion> solucionLineal(int nuDiasEscanear, Libreria [] librerias, int [] puntacionLibro){
+		List<LibreriaSolucion> solucion = new ArrayList<LibreriaSolucion>();
+		Set<Integer> librosEscaneados= new HashSet<Integer>();
+		List<Libreria> libs = Arrays.asList(librerias);
+		//Ordenamos las librerias 
+		libs = libs.stream()
+				.sorted((o1, o2) -> o1.getNuDiasSignUp() - o2.getNuDiasSignUp())
+				.collect(Collectors.toList());
+		
+		libs.forEach(l->{
+			for(int i = 0; i<l.getLibros().length;i++) {
+				if(!librosEscaneados.contains(l.getLibros()[i])) {
+					LibreriaSolucion ls = new LibreriaSolucion();
+					ls.setIndiceLibreria(l.getIndiceLibreria());
+					ls.setLibros(l.getLibros());
+					ls.setNuLibrosEnviados(l.getLibros().length);
+					solucion.add(ls);
+					break;
+				}
+			}
+		});
+		
+		return solucion;
+	}
+	
+	
 }
